dce640a13cca5104fe4d8ed37715c954c2302f1d
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-03-25 09:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert_Row() {
     const table = document.getElementById("sampleTable");
 const tr = document.createElement("tr");
-tr.innerHTML += `<td>New Cell1</td>`;
-tr.innerHTML += `<td>New Cell2</td>`;
+tr.innerHTML += "<td>New Cell1</td>";
+tr.innerHTML += "<td>New Cell2</td>";
  
 	table.appendChild(tr);
   

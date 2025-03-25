9762917ad2839c436cead743c640a11109fac39d
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-03-25 10:08:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function insert_Row() {
     const table = document.getElementById("sampleTable");
+    const tbody = table.tBodies[0];  // get the first <tbody> element in the table
     const tr = document.createElement("tr");
     tr.innerHTML += `<td>New Cell1</td>`;
     tr.innerHTML += `<td>New Cell2</td>`;
     
-    // insert the new row at the top of the table
-    table.insertBefore(tr, table.firstChild);
+    // insert the new row at the top of the tbody
+    tbody.insertBefore(tr, tbody.firstChild);
 }
